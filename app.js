e8de08374430369a872011505c663693d206b426
LizaAkishina <lizaakishina@gmail.com>
2022-09-23 11:04:13 +0300

    fix: исправлен файл app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '6326ce1a7703c391bd86530f'
+    _id: '6326ce1a7703c391bd86530f',
   };
 
   next();
